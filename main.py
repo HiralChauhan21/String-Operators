2f12b6ea78a11ed7231ed3b400479b39e24a59e6
HiralChauhan21 <vishal.chauhan.home@gmail.com>
2024-11-10 14:35:49 +0530

    commit

---
+++ main.py
@@ -0,0 +1,15 @@
+first_name= "Codingal"
+last_name= "educations"
+full_name= first_name+last_name
+example= "haa"*5
+
+print("frist name is ", first_name)
+print("last name is ", last_name)
+print("full name is ", full_name)
+print("string multiplied 5 times give ", example)
+
+word= "coding"
+print("length of word is", len(word))
+print("the first letter is ", word[0])
+print("the last letter is ", word[5])
+print("string sliced: ", word[0:3])
